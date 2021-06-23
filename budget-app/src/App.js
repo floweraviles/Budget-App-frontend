6d4bd6dce252f583991a41272118c2ac02c64200
Flower Aviles <itsfloweraviles@gmail.com>
2021-06-23 19:52:40 -0400

    frontend setup

---
+++ budget-app/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
